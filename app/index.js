981c39b0f424f3eb7137eba83f4f25a4b4435793
jeanffc <jeanffc@gmail.com>
2020-03-26 10:03:05 -0300

    feat: redux

--- app/index.js
+++ app/index.js
@@ -1,11 +1,16 @@
-import '@babel/polyfill';
+import "@babel/polyfill";
 
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
 
-import App from './components/App';
+import store from "./store";
+
+import App from "./components/App";
 
 render(
-    <App />,
-    document.querySelector('#root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
 );
